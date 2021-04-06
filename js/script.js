2734a65a1fcc70bdf18761b5c15a33a7face3bc8
Ansar <example@gmail.com>
2021-04-06 17:36:54 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// console.log(4++);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isCheked = false,
+      isClose = false;
+
+console.log(isCheked || !isClose);
